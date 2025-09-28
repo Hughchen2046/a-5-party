40932b2cec3138ed6993ec50172e5391b806acc3
Hughchen2046 <hughchen2046@gmail.com>
2025-09-28 13:02:10 +0800

    ok

--- main.js
+++ main.js
@@ -3,31 +3,58 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 // 新增swiper
 var headerswiper = new Swiper(".headerSwiper", {
-      spaceBetween: 30,
-      effect: "fade",
-      loop: true,
-      lazy: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },    
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + "</span>";
-        },
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  lazy: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "</span>";
     },
+  },
 });
 
-    var indexswiper = new Swiper(".indexSwiper", {
-      slidesPerView: "auto",
-      lazy: true,
-      freeMode: true,
-      spaceBetween: 12,
-      breakpoints: {
-        768: {
-          spaceBetween: 24,
-        },
-      },
-    });
+var indexswiper = new Swiper(".indexSwiper", {
+  slidesPerView: "auto",
+  lazy: true,
+  freeMode: true,
+  spaceBetween: 12,
+  breakpoints: {
+    768: {
+      spaceBetween: 24,
+    },
+  },
+});
+
+var contactsoonswiper = new Swiper(".contactsoonSwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  lazy: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "</span>";
+    },
+  },
+});
+
+
+var feedbackswiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: "auto",
+  lazy: true,
+  freeMode: true,
+  spaceBetween: 24,
+
+});
